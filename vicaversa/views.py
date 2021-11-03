d7afdc585453b27648f0948879cc9a79b706b259
candarali <cahandarslim@gmail.com>
2021-11-03 22:39:13 +0400

    third commit but with minor change

--- vicaversa/views.py
+++ vicaversa/views.py
@@ -6,4 +6,5 @@ def home(request):
 
 def reverse(request):
 	user_text = request.GET['usertext']
-	return render(request,'reverse.html',{'usertext':user_text})
+	reversed_text = user_text[::-1]
+	return render(request,'reverse.html',{'usertext':user_text, 'reversed_text':reversed_text})
